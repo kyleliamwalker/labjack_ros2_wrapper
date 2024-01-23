1a294767a180525ee74ed7f2d609e6b4e8cf022c
Kyle Walker <walker12@live.co.uk>
2024-01-23 16:23:21 +0000

    set to read 2 encoders

--- src/read_quad_encoder.py
+++ src/read_quad_encoder.py
@@ -21,6 +21,9 @@ class LabJack( Node ):
         ljm.eWriteNames(self.lj_handle, 6, ["DIO0_EF_ENABLE", "DIO1_EF_ENABLE", "DIO0_EF_INDEX", 
                                             "DIO1_EF_INDEX", "DIO0_EF_ENABLE", "DIO1_EF_ENABLE"], 
                                             [0, 0, 10, 10, 1, 1])
+        ljm.eWriteNames(self.lj_handle, 6, ["DIO2_EF_ENABLE", "DIO3_EF_ENABLE", "DIO2_EF_INDEX", 
+                                            "DIO3_EF_INDEX", "DIO2_EF_ENABLE", "DIO3_EF_ENABLE"], 
+                                            [0, 0, 10, 10, 1, 1])
 
         # init timer to publish encoder position
         timer_period = 0.01 # seconds
@@ -31,7 +34,7 @@ class LabJack( Node ):
 
         # DIO0_EF_READ_A_F displays counts both positive and negative
         # DIO0_EF_READ_A displays counts positive and negative as two's complement
-        data = ljm.eReadNames(self.lj_handle, 2, ["DIO0_EF_READ_A_F", "DIO0_EF_READ_A"])
+        data = ljm.eReadNames(self.lj_handle, 2, ["DIO0_EF_READ_A", "DIO2_EF_READ_A"])
         # self.get_logger().info("AIN0 : %f V, AIN1 : %f V" % (data[0], data[1]))
 
         # pub msg
@@ -43,11 +46,11 @@ class LabJack( Node ):
         encoder_msg.layout.dim = [MultiArrayDimension(), MultiArrayDimension()]
 
         # dim[0] is AIN0
-        encoder_msg.layout.dim[0].label = "DIO0_AF"
+        encoder_msg.layout.dim[0].label = "DIO0_A"
         encoder_msg.layout.dim[0].size = 1
         encoder_msg.layout.dim[0].stride = 1
         # dim[1] is AIN1
-        encoder_msg.layout.dim[1].label = "DIO0_A"
+        encoder_msg.layout.dim[1].label = "DIO2_A"
         encoder_msg.layout.dim[1].size = 1
         encoder_msg.layout.dim[1].stride = 1
 
